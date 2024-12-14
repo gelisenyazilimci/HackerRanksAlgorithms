38db5092b61a21db6acc4ee5f448b496660173d4
gelisenyazilimci <burbytes@gmail.com>
2024-12-14 07:55:33 +0300

    Staircase algorithms solution

---
+++ Staircase/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        for (int row = 0; row < n; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                if (col < n - row - 1)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("#");
+                }
+            }
+            Console.WriteLine(); // Her satırın sonunda yeni satıra geçmek için eklenmeli
+        }
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
